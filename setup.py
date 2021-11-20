169e70a0bf5dbfc5e9b63aff51c83fcb0f34f8a2
Tim <94218704+predictiveauditing@users.noreply.github.com>
2021-11-20 15:19:46 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+import os
+# os.system('pip install git+https//github.com/timbaessler/ConformancePandas.git@master')
+
+setuptools.setup(name='conformancepandas',
+				 version="0.0.1",
+				 author="Tim Bäßler",
+				 author_email="tim.baessler@web.de",
+				 url="https://github.com/timbaessler/ConformancePandas",
+				 packages=["conformancepandas"],
+				 install_requires=["pm4py==2.2.16"])
