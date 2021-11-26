3d485f2014547b7e6fd739da72994e056e027197
Tim <predictiveauditing@gmail.com>
2021-11-26 16:53:49 +0100

    update name

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
 import os
-# os.system('pip install git+https//github.com/timbaessler/ConformancePandas.git@master')
 
 setuptools.setup(name='conformancelabeler',
 				 version="0.0.1",
 				 author="Tim Bäßler",
 				 author_email="tim.baessler@web.de",
-				 url="https://github.com/timbaessler/ConformancePandas",
+				 url="https://github.com/timbaessler/conformance-labeler",
 				 packages=["conformancelabeler"],
 				 install_requires=["pm4py", "pandas"])
