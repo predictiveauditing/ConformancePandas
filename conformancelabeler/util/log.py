d5a27330b5887671c4ae8fc7a604c5f8efb3799b
Tim <predictiveauditing@gmail.com>
2022-01-29 12:00:45 +0100

    Update log.py

--- conformancelabeler/util/log.py
+++ conformancelabeler/util/log.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.conversion.log import converter as log_converter
-
+import os
+import pickle
 
 
 def read_xes(file: str):
@@ -35,10 +36,37 @@ def to_categorical(log: pd.DataFrame,
 		log[col] = pd.factorize(log[col])[0] +1
 	return log
 
-
 def scale_numerical_features(log: pd.DataFrame, num_cols: list):
 	log[num_cols] = log[num_cols].astype(float)
 	log[num_cols] = (log[num_cols] - log[num_cols].mean()) / log[num_cols].std()
 	return log
 
+def num_embeddings(log, cat_cols: list)->list:
+	return list([log[col].nunique() for col in cat_cols])
 
+def to_pickle(log, processed_path, num_cols, cat_cols,
+		   case_id_col='case:concept:name', target_col='y'):
+
+	case_list = log[case_id_col].unique().tolist()
+
+	targets = list()
+	x_num = list()
+	x_cat = list()
+
+	j = 0
+	for case_id in case_list:
+		case_data = log[log[case_id_col]==case_id]
+		label = case_data.groupby(case_id_col)[target_col].first().values.tolist()
+		targets += label
+		case_data = case_data.drop(columns=[case_id_col, target_col])
+		x_num.append(case_data[num_cols].values.astype(float).tolist())
+		x_cat.append(case_data[cat_cols].values.astype(int).tolist())
+		j += 1
+
+		if j % 10000 == 0:
+			print("progress: ", round(j/len(case_list)*100, 2), "% with ",j, " cases processed")
+			np.save(file=os.path.join(processed_path, 'targets.npy'), arr=targets)
+			with open(os.path.join(processed_path, 'x_num.pkl'), 'wb') as f:
+				pickle.dump(x_num, f)
+			with open(os.path.join(processed_path, 'x_cat.pkl'), 'wb') as f:
+				pickle.dump(x_cat, f)
